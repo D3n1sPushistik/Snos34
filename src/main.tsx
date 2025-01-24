0e13b10e7f80b8a90deffae4ce6c459c479cb4bf
D3n1sPushistik <denisignatkin471@gmail.com>
2025-01-25 02:28:00 +0300

    24.01-v2
    1. Добавлена анимация появления блоков с названием работ в блоке 2.
    2. Добавлены полосы пути по этапам работы, прикреплены к центру кружков, стиль соблюдён.
    3. В кружки этапов работ добавлены значки, характеризующие их деятельность.
    4. Кружки красиво оформлены.
    5. Текст отформатирован по красоте.
    6. Добавлен универсальный шаблон определения мобильности устройства
    7. Добавлен градиентный фон и декоративные элементы для блока 3
    8. Добавлен футер.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BreakpointProvider } from './contexts/BreakpointContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BreakpointProvider>
+      <App />
+    </BreakpointProvider>
   </StrictMode>
 );
